7678509e87e953182fe4a05b9341d2b7833610f3
Sharina Stubbs <sharinastubbs@gmail.com>
2019-03-20 16:20:43 -0700

    Added POST, PUT, PATCH, DELETE functions to APIView

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -3,19 +3,53 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-# Create your views here.
-class HelloApiView(APIView):
-    """Test API View"""
+from . import serializers
+from rest_framework import status
 
-    def get(self,request, format=None):
-        """Returns a list of APIView features"""
+# Views
+
+class HelloApiView(APIView):
+    """Test API View."""
+
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features."""
 
         an_apiview = [
             'Uses HTTP methods as function (get, post, patch, put, delete)',
             'It is similar to a traditional Django view',
             'Gives you the most control over your logic',
-            'is mapped manually to URLs'
-
+            'Is mapped manually to URLs'
         ]
 
-        return Response({'message': 'Hello!', 'an_apiview': an_apiview}) # must be a dictionary format, which is then converted to JSON.
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with a name."""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}!'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self, request, pk=None):
+        """Handles updating a specific object"""
+
+        return Response({'method': 'put'})
+
+    def patch(self, request, pk=None):
+        """Patch request, only updates fields provided in the request"""
+
+        return Response({'method': 'patch'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+
+        return Response({'method': 'delete'})
